e64889561b7355ece6d107f528e0ea20359da02b
mira-jay <mirajayant@gmail.com>
2026-01-17 18:38:34 -0600

    new xarm test

--- xarm/xarm_test7.py
+++ xarm/xarm_test7.py
@@ -4,19 +4,21 @@ blue = xarm.Controller("USBD30F103095D182300023D4D4")
 
 #set servos for blue
 blue_servo2 = xarm.Servo(2)
-blue_servo3 = xarm.Servo(3, 100)
-blue_servo4 = xarm.Servo(4, 483)
+blue_servo3 = xarm.Servo(3, 870)
+blue_servo4 = xarm.Servo(4, 570)
 print("servo4: ", blue.getPosition(blue_servo4))
-blue_servo5 = xarm.Servo(5)
+blue_servo5 = xarm.Servo(5, 500)
 print("servo5: ", blue.getPosition(blue_servo5))
-blue_servo6 = xarm.Servo(6, 883)
+blue_servo6 = xarm.Servo(6, 150)
 
 
-#blue.setPosition([blue_servo2, blue_servo3, blue_servo4, blue_servo5, blue_servo6], wait=True)
-blue.setPosition(5, 500, wait=True)
+blue.setPosition([blue_servo2, blue_servo3, blue_servo4, blue_servo5, blue_servo6], wait=True)
+blue.setPosition(5, 1000, wait=True)
 print("servo5: ", blue.getPosition(blue_servo5))
-blue.setPosition(4, 500, wait=True)
+blue.setPosition(6, 540, wait=True)
 print("servo4: ", blue.getPosition(blue_servo4))
-#blue.setPosition(5, 500, wait = True)
-#blue.setPosition(6, 103, wait = True)
+blue.setPosition(4, 830, wait = True)
+blue.setPosition(3, 138, wait = True)
 #blue.setPosition(4, 876, wait=True)
+
+
---
+++ xarm/xarm_test8.py
@@ -0,0 +1,27 @@
+import xarm
+
+blue = xarm.Controller("USBD30F103095D182300023D4D4")
+
+#set servos for blue
+blue_servo2 = xarm.Servo(2)
+blue_servo3 = xarm.Servo(3)
+blue_servo4 = xarm.Servo(4)
+print("servo4: ", blue.getPosition(blue_servo4))
+blue_servo5 = xarm.Servo(5)
+print("servo5: ", blue.getPosition(blue_servo5))
+blue_servo6 = xarm.Servo(6)
+
+#grab the patch
+blue.setPosition(5, 500, wait=True)
+blue.setPosition(6, 150, wait=True)
+blue.setPosition(4, 830, wait = True)
+blue.setPosition(3, 138, wait = True)
+
+#take it out
+blue.setPosition(3, 870, wait = True)
+blue.setPosition(4, 570, wait = True)
+blue.setPosition(6, 540, wait = True)
+blue.setPosition(5, 1000, wait = True)
+
+
+
